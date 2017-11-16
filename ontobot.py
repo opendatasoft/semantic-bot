ff636c916b3e85c7293f2c9689148984bb0ccc3d
benjimor <benji_moreau@hotmail.fr>
2017-11-16 12:21:47 +0100

    ontobot.py now takes the dataset_id parameter

--- ontobot.py
+++ ontobot.py
@@ -1,5 +1,11 @@
+import sys
+
 from utils.lov_api import LovApi
 from utils.ods_catalog_api import ODSCatalogApi
 
-print ODSCatalogApi.dataset_meta_request('roman-emperors@public')
-print ODSCatalogApi.dataset_meta_request('roman')
+if len(sys.argv) > 1:
+    print ODSCatalogApi.dataset_meta_request(sys.argv[1])
+    sys.exit()
+else:
+    print 'ontobot.py <dataset_id>'
+    sys.exit()
