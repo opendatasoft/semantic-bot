5d13072cf469177832dac9e1e5367538435edc63
benjimor <benji_moreau@hotmail.fr>
2017-11-16 17:53:33 +0100

    use of nltk and bs to make some natural language processing

--- ontobot.py
+++ ontobot.py
@@ -2,9 +2,12 @@ import sys
 
 from utils.lov_api import LovApi
 from utils.ods_catalog_api import ODSCatalogApi
+from utils.natural_language_processor import NLProcessor
 
 if len(sys.argv) > 1:
-    print ODSCatalogApi.dataset_meta_request(sys.argv[1])
+    dataset = ODSCatalogApi.dataset_meta_request(sys.argv[1])
+    description = dataset['metas']['description']
+    print NLProcessor.extract_noun(description)
     sys.exit()
 else:
     print 'ontobot.py <dataset_id>'
---
+++ utils/natural_language_processor.py
@@ -0,0 +1,27 @@
+from nltk import word_tokenize, WordNetLemmatizer, pos_tag
+from nltk.corpus import stopwords
+from bs4 import BeautifulSoup
+
+
+class NLProcessor(object):
+    """
+    This class implements Natural Language Processing used by the Ontology ChatBot.
+    It use BeautifulSoup to extract text from HTML and NLTK for Natural Language Processing
+    """
+
+    @staticmethod
+    def extract_noun(text):
+        """Return a list with singular and plural noun."""
+        stop_words = set(stopwords.words('english'))
+        text = BeautifulSoup(text, 'html.parser').get_text()
+        text = word_tokenize(text)
+        filtered_text = []
+        for word in text:
+            if word not in stop_words:
+                filtered_text.append(word)
+        filtered_text = pos_tag(filtered_text)
+        text_nouns = []
+        for item in filtered_text:
+            if item[1][0] == 'NNS' or item[1] == 'NNP':
+                text_nouns.append(item[0])
+        print text_nouns
