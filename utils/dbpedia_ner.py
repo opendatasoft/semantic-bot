d8c57a790bb03016d70330a14935344c5c0e44e6
benjimor <benji_moreau@hotmail.fr>
2018-02-23 22:39:10 +0100

    dbpedia french version

--- utils/dbpedia_ner.py
+++ utils/dbpedia_ner.py
@@ -4,21 +4,24 @@ DBPEDIA_RESOURCE_URI = "http://dbpedia.org/resource/{resource_name}"
 DBPEDIA_ONTOLOGY_URI = "http://dbpedia.org/ontology/"
 
 try:
-    eng_dbpedia = HDTDocument("dbpedia_dump/instance_type.hdt")
+    eng_dbpedia = HDTDocument("dbpedia_dump/en/instance_type.hdt")
 except RuntimeError:
-    raise RuntimeError("DBpedia dump not found: Put dbpedia dump into dbpedia_dump folder")
+    raise RuntimeError("DBpedia english dump not found: Put dbpedia dump into dbpedia_dump folder")
+try:
+    fr_dbpedia = HDTDocument("dbpedia_dump/fr/instance_type.hdt")
+except RuntimeError:
+    raise RuntimeError("DBpedia french dump not found: Put dbpedia dump into dbpedia_dump folder")
 
 
 def entity_types_request(query, lang='en'):
     if query:
         query = query.encode("utf-8")
-        query = query.title()
-        query = query.replace(' ', '_')
+        query = to_dbpedia_format(query)
         subject_query = DBPEDIA_RESOURCE_URI.format(resource_name=query)
-        if lang == 'en':
-            (triples, cardinality) = eng_dbpedia.search_triples(subject_query, "", "")
-        else:
-            (triples, cardinality) = eng_dbpedia.search_triples(subject_query, "", "")
+        (triples, cardinality) = eng_dbpedia.search_triples(subject_query, "", "")
+        if cardinality < 0:
+            (triples, cardinality) = fr_dbpedia.search_triples(subject_query, "", "")
+            print "FR"
         classes = []
         for triple in triples:
             cl = triple[2]
@@ -26,6 +29,11 @@ def entity_types_request(query, lang='en'):
                 cl = cl.replace(DBPEDIA_ONTOLOGY_URI, '')
                 classes.append(cl)
         if classes:
-            print classes
             return classes
     return None
+
+
+def to_dbpedia_format(query):
+    query = query.title()
+    query = query.replace(' ', '_')
+    return query
