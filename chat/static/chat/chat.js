0601cba6abcc2009246609bef2d7defd513267a2
benjimor <benji_moreau@hotmail.fr>
2018-03-15 17:57:35 +0100

    added d3js-graph to illustrate the mapping

--- chat/static/chat/chat.js
+++ chat/static/chat/chat.js
@@ -14,7 +14,6 @@ var chat = new Vue({
   },
   mounted: function(){
       this.bot_introduction();
-
   },
   methods: {
     push_user_message: function (message) {
@@ -70,10 +69,10 @@ var chat = new Vue({
     },
     user_input_yes: function () {
       if (this.awaiting_user) {
+        this.awaiting_user = false;
         this.push_user_message("Yes.");
         this.$http.get('/api/conversation/answer/positive').then(response => {
           this.push_bot_message(response.body['text']);
-          this.awaiting_user = false;
           if ((this.current_correspondance_type == 'properties') && (this.confirmed_correspondances['classes'].length > 0)) {
             this.$http.post('/api/conversation/question/property-class', this.current_correspondance).then(response => {
               this.push_bot_message(response.body['text']);
@@ -82,6 +81,7 @@ var chat = new Vue({
             });
           } else {
             this.confirmed_correspondances[this.current_correspondance_type].push(this.current_correspondance);
+            update_graph(this.current_correspondance, this.current_correspondance_type);
             setTimeout(function(){chat.next_semantize()},1500);
           }
         })
@@ -89,10 +89,10 @@ var chat = new Vue({
     },
     user_input_idk: function () {
       if (this.awaiting_user) {
+        this.awaiting_user = false;
         this.push_user_message("I don't know.");
         this.$http.get('/api/conversation/answer/neutral').then(response => {
           this.push_bot_message(response.body['text']);
-          this.awaiting_user = false;
           this.awaiting_correspondances[this.current_correspondance_type].push(this.current_correspondance);
           setTimeout(function(){chat.next_semantize()},1500);
         });
@@ -100,10 +100,10 @@ var chat = new Vue({
     },
     user_input_no: function () {
       if (this.awaiting_user) {
+        this.awaiting_user = false;
         this.push_user_message("No.");
         this.$http.get('/api/conversation/answer/negative').then(response => {
           this.push_bot_message(response.body['text']);
-          this.awaiting_user = false;
           this.denied_correspondances[this.current_correspondance_type].push(this.current_correspondance);
           setTimeout(function(){chat.next_semantize()},1500);
         });
@@ -112,10 +112,10 @@ var chat = new Vue({
     user_input_property_class: function (associated_class) {
       if ((this.awaiting_user) && (!this.yes_no_questions)) {
         if (associated_class == null){
+          this.awaiting_user = false;
           this.push_user_message('None of those');
           this.$http.get('/api/conversation/answer/negative').then(response => {
             this.push_bot_message(response.body['text']);
-            this.awaiting_user = false;
             this.current_correspondance['associated_class'] = [];
             this.current_correspondance['associated_field'] = [];
             this.denied_correspondances[this.current_correspondance_type].push(this.current_correspondance);
@@ -123,14 +123,15 @@ var chat = new Vue({
             setTimeout(function(){chat.next_semantize()},1500);
           });
         } else {
+          this.awaiting_user = false;
           this.push_user_message(associated_class['class']);
           this.$http.get('/api/conversation/answer/positive').then(response => {
             this.push_bot_message(response.body['text']);
-            this.awaiting_user = false;
             this.current_correspondance['associated_class'] = associated_class['class'];
             this.current_correspondance['associated_field'] = associated_class['field_name'];
             this.confirmed_correspondances[this.current_correspondance_type].push(this.current_correspondance);
             this.yes_no_questions = true;
+            update_graph(this.current_correspondance, this.current_correspondance_type);
             setTimeout(function(){chat.next_semantize()},1500);
           });
         }
@@ -138,3 +139,176 @@ var chat = new Vue({
     },
   },
 });
+
+// D3JS part for the graph
+var nodes = [];
+var links = [];
+
+var colors = d3.scaleOrdinal(d3.schemeCategory10);
+
+var svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height"),
+    node,
+    link;
+
+var simulation = d3.forceSimulation()
+    .force("link", d3.forceLink().id(function (d) {return d.id;}).distance(100).strength(1))
+    .force("charge", d3.forceManyBody().strength(-25).distanceMax(100))
+    .force("center", d3.forceCenter(0, 0));
+
+update(links, nodes);
+
+function update(links, nodes) {
+    svg.selectAll("*").remove();
+
+    svg.append('defs').append('marker')
+        .attrs({'id':'arrowhead',
+            'viewBox':'-0 -5 10 10',
+            'refX':30,
+            'refY':0,
+            'orient':'auto',
+            'markerWidth':13,
+            'markerHeight':13,
+            'xoverflow':'visible'})
+        .append('svg:path')
+        .attr('d', 'M 0,-5 L 10 ,0 L 0,5')
+        .attr('fill', '#999')
+        .style('stroke','none');
+
+    link = svg.selectAll(".link")
+        .data(links)
+        .enter()
+        .append("line")
+        .attr("class", "link")
+        .attr('marker-end','url(#arrowhead)')
+
+    link.append("title")
+        .text(function (d) {return d.type;});
+
+    edgepaths = svg.selectAll(".edgepath")
+        .data(links)
+        .enter()
+        .append('path')
+        .attrs({
+            'class': 'edgepath',
+            'fill-opacity': 0,
+            'stroke-opacity': 0,
+            'id': function (d, i) {return 'edgepath' + i}
+        })
+        .style("pointer-events", "none");
+
+    edgelabels = svg.selectAll(".edgelabel")
+        .data(links)
+        .enter()
+        .append('text')
+        .style("pointer-events", "none")
+        .attrs({
+            'class': 'edgelabel',
+            'id': function (d, i) {return 'edgelabel' + i},
+            'font-size': 12,
+            'fill': '#aaa'
+        });
+
+    edgelabels.append('textPath')
+        .attr('xlink:href', function (d, i) {return '#edgepath' + i})
+        .style("text-anchor", "middle")
+        .attr("class", "noselect")
+        .attr("startOffset", "50%")
+        .text(function (d) {return d.label});
+
+    node = svg.selectAll(".node")
+        .data(nodes)
+        .enter()
+        .append("g")
+        .attr("class", "node")
+        .call(d3.drag()
+                .on("start", dragstarted)
+                .on("drag", dragged)
+        );
+
+    node.append("circle")
+        .attr("r", 30)
+        .style("fill", function(d) { return (d.group == "resource") ? "#a5b6d3" : "#E8E8E8"; })
+
+    node.append("title")
+        .text(function (d) {return d.id;});
+
+    node.append("text")
+        .attr("font-size","10")
+        .attr("class", "noselect")
+        .attr("text-anchor", "middle")
+        .attr("dy", 0)
+        .append("tspan")
+          .text(function (d) {return d.label;});
+    node.select("text").append("tspan")
+      .attr("x", "0")
+      .attr("dy", "1em")
+      .text(function (d) {return "(" + d.id + ")";});
+
+    simulation
+        .nodes(nodes)
+        .on("tick", ticked);
+
+    simulation.force("link")
+        .links(links);
+}
+
+function ticked() {
+    link
+        .attr("x1", function (d) {return d.source.x;})
+        .attr("y1", function (d) {return d.source.y;})
+        .attr("x2", function (d) {return d.target.x;})
+        .attr("y2", function (d) {return d.target.y;});
+
+    node
+        .attr("transform", function (d) {return "translate(" + d.x + ", " + d.y + ")";});
+
+    edgepaths.attr('d', function (d) {
+        return 'M ' + d.source.x + ' ' + d.source.y + ' L ' + d.target.x + ' ' + d.target.y;
+    });
+
+    edgelabels.attr('transform', function (d) {
+        if (d.target.x < d.source.x) {
+            var bbox = this.getBBox();
+
+            rx = bbox.x + bbox.width / 2;
+            ry = bbox.y + bbox.height / 2;
+            return 'rotate(180 ' + rx + ' ' + ry + ')';
+        }
+        else {
+            return 'rotate(0)';
+        }
+    });
+}
+
+function dragstarted(d) {
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart()
+    d.fx = d.x;
+    d.fy = d.y;
+}
+
+function dragged(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+}
+
+function update_graph(correspondance, correspondance_type){
+  if (correspondance_type == "classes"){
+    nodes.push({id: correspondance.field_name, label: correspondance.class, group: 'resource'});
+  } else {
+    nodes.push({id: correspondance.field_name, label: 'field value', group: 'value'});
+    links.push({source: correspondance.associated_field, target: correspondance.field_name, label: correspondance.description})
+  }
+  update(links, nodes);
+  simulation.alphaTarget(0.3).restart()
+}
+
+var chart = $("#chart"),
+  aspect = chart.width() / chart.height(),
+  container = chart.parent();
+$(window).on("resize", function() {
+  var targetWidth = container.width();
+  chart.attr("width", targetWidth);
+  chart.attr("height", Math.round(targetWidth / aspect));
+}).trigger("resize");
