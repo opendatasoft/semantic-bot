c0de20ebbd8e6bc4955e25898a2936c435b6aacf
benjimor <benji_moreau@hotmail.fr>
2018-08-22 13:11:37 +0200

    about fixed with nicolas.T

--- chat/static/chat/about.js
+++ chat/static/chat/about.js
@@ -14,27 +14,36 @@ document.write(`
           OpenDataSoft is an online platform designed to quickly facilitate the publication,
           sharing and re-use of data by business users and stimulate the creation of new applications.
         <br>
-          More infos on OpenDataSoft <a href="https://www.opendatasoft.com/">here</a>.
+          <a href="https://www.opendatasoft.com/">www.opendatasoft.com</a>.
         <br>
-          Discover OpenDataSoft's data network <a href="https://data.opendatasoft.com/">here</a>.
+        </p>
+        <p>
+          Data network is a dataset library accesible for everyone. It gathers public datasets from all users in one place.
+        <br>
+          <a href="https://data.opendatasoft.com/">data.opendatasoft.com</a>.
         </p>
         <h3> Semantic </h3>
         <p>
-          <a href="https://en.wikipedia.org/wiki/Semantic_Web">Semantic web</a> is a web where data is described
-          using common representation <a href="https://en.wikipedia.org/wiki/Resource_Description_Framework">(RDF)</a> and vocabularies.
-          It allows data to be reused accross application, enterprise, city, country, ...
+          <a href="https://en.wikipedia.org/wiki/Semantic_Web">Semantic web</a> technologies are used on OpenDataSoft platform
+          to improve data interoperability. It facilitates data discoverability and data consumption.
+          Semantic powered features such as semantic filters or automatic federation of datasets will make OpenDataSoft plateform smarter.
+        </p>
+        <p>
+          A documentation about how semantic web is used on OpenDataSoft platform is
+          available at <a href="hhttps://help.opendatasoft.com/apis/tpf/">help.opendatasoft.com/apis/tpf</a>.
         </p>
         <h3> Chatbot </h3>
         <p>
           Integrating data in the semantic web is not an easy task.
           The goal of this chatbot is to assist you in this process.
         <br>
-          This tool is powered with linked data from <a href="http://lov.okfn.org/">Linked Open Vocabularies</a>
-          and <a href="https://wiki.dbpedia.org/">DBpedia</a>.
+          Chatbot will generate an <a href="http://rml.io/">RML</a>. mapping file that can be uploaded to OpenDataSoft plateform to define how your dataset should be described in semantic formats.
         <br>
-          A documentation about how semantic web is used on OpenDataSoft platform is
-          available <a href="https://docs.opendatasoft.com/api/explore/tpf.html">here</a>.
+          This tool is powered with linked data from
+          <a href="http://lov.okfn.org/">Linked Open Vocabularies</a>, <a href="https://wiki.dbpedia.org/">DBpedia</a> and <a href="https://www.wikidata.org/">Wikidata</a>.
         </p>
+        <h3> Contact </h3>
+        <a href="TO DO">Hubspot</a>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
