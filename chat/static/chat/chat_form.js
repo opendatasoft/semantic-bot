c076672fa3aa3b9ad8c8b247e7a5320af995817e
benjimor <benji_moreau@hotmail.fr>
2018-10-29 11:47:38 +0100

    autocomplete use dataset title and id

--- chat/static/chat/chat_form.js
+++ chat/static/chat/chat_form.js
@@ -2,35 +2,40 @@ var chat_form = new Vue({
   el: '#chat-form',
   data: {
     ods_catalog_url: "https://data.opendatasoft.com/api/datasets/1.0/",
-    ods_suggestion_url: "https://data.opendatasoft.com/api/v2/catalog/datasets?rows=5&pretty=false&timezone=UTC&include_app_metas=false&where=datasetid%09%20like%20",
+    ods_suggestion_url: "https://data.opendatasoft.com/api/v2/catalog/datasets?rows=10&pretty=false&timezone=UTC&include_app_metas=false&sort=explore.popularity_score&where=",
     suggestions: [],
-    minimum_char_to_suggest: 2,
+    minimum_char_to_suggest: 3,
     suggestion_timeout_ms: 1000
   },
   watch: {
     suggestions: function (val) {
       $('#textBar').autocomplete({
-          lookup: chat_form.suggestions
+          lookup: chat_form.suggestions,
+          onSelect: function (suggestion) {
+              $('#textBar').val(suggestion.data);
+          }
       });
-      $('#textBar').focusout()
-      $('#textBar').focus()
+      $('#textBar').focusout();
+      $('#textBar').focus();
     }
   },
   methods: {
     dataset_exists: function (dataset) {
       this.$http.get(this.ods_catalog_url + dataset).then(response => {
-        document.getElementById("textBar").classList.remove("is-invalid")
+        document.getElementById("textBar").classList.remove("is-invalid");
         window.location = "/chatbot/" + dataset;
       },response => {
-        document.getElementById("textBar").classList.add("is-invalid")
-        return false
+        document.getElementById("textBar").classList.add("is-invalid");
+        return false;
       });
     },
     get_suggestions: function (query) {
-      this.$http.get(this.ods_suggestion_url + "%22" + query +"%22").then(response => {
-        ods_suggestions = []
+      this.$http.get(this.ods_suggestion_url + 'title%20like%20%22' + query + '%22%20OR%20datasetid%20like%20%22' + query + '%22').then(response => {
+        ods_suggestions = [];
         for (var i = 0; i < response.body['datasets'].length; i++){
-          ods_suggestions.push({"value": response.body['datasets'][i]['dataset']['dataset_id']});
+          title = response.body['datasets'][i]['dataset']['metas']['default']['title'];
+          dataset_id = response.body['datasets'][i]['dataset']['dataset_id'];
+          ods_suggestions.push({"value": title + ' [' + dataset_id + ']', "data": dataset_id});
         }
         this.suggestions = ods_suggestions;
       },response => {
@@ -44,7 +49,7 @@ $(function(){
   $('.Global-container').keypress(function(e){
     if(e.which == 13) {
       dataset = document.getElementById("textBar").value;
-      chat_form.dataset_exists(dataset)
+      chat_form.dataset_exists(dataset);
     }
   })
 });
@@ -56,7 +61,7 @@ $('#textBar').on('input',function(e){
         clearTimeout(timeout);
   }
   timeout = setTimeout(function () {
-    var textbar_value = $("#textBar").val()
+    var textbar_value = $("#textBar").val();
     if (textbar_value.length > chat_form.minimum_char_to_suggest){
       chat_form.get_suggestions(textbar_value);
     }
