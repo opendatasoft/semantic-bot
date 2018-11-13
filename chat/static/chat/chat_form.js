5679c379a5933047f0e07ea935f08d28886f2e39
benjimor <benji_moreau@hotmail.fr>
2018-11-13 11:35:10 +0100

    add portal icon to suggestions in dataset search bar

--- chat/static/chat/chat_form.js
+++ chat/static/chat/chat_form.js
@@ -1,3 +1,30 @@
+// Overide jquery autocomplete format result to support domain favicon
+$.Autocomplete.defaults.formatResult = function _formatResult(suggestion, currentValue) {
+    // Do not replace anything if the current value is empty
+    if (!currentValue) {
+        return suggestion.value + ' [' + suggestion.data + ']';
+    }
+
+    var utils = $.Autocomplete.utils
+
+    var pattern = '(' + utils.escapeRegExChars(currentValue) + ')';
+
+    return '<img src="http://' + suggestion.favicon + '" class="portal-icon"> ' + suggestion.value
+        .replace(new RegExp(pattern, 'gi'), '<strong>$1<\/strong>')
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/&lt;(\/?strong)&gt;/g, '<$1>')
+        + ' [<b>' + suggestion.data
+        .replace(new RegExp(pattern, 'gi'), '<strong>$1<\/strong>')
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/&lt;(\/?strong)&gt;/g, '<$1>') + '</b>]';
+};
+
 var chat_form = new Vue({
   el: '#chat-form',
   data: {
@@ -35,7 +62,8 @@ var chat_form = new Vue({
         for (var i = 0; i < response.body['datasets'].length; i++){
           title = response.body['datasets'][i]['dataset']['metas']['default']['title'];
           dataset_id = response.body['datasets'][i]['dataset']['dataset_id'];
-          ods_suggestions.push({"value": title + ' [' + dataset_id + ']', "data": dataset_id});
+          favicon = response.body['datasets'][i]['dataset']['metas']['default']['source_domain_address'] + '/favicon.ico'
+          ods_suggestions.push({"favicon": favicon, "value": title, "data": dataset_id});
         }
         this.suggestions = ods_suggestions;
       },response => {
