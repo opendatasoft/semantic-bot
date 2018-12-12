34231b3c598443e7bbf3562e4430ef74ae35b201
benjimor <benji_moreau@hotmail.fr>
2018-12-12 15:56:02 +0100

    install bash file working

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@ pip install pybind11==2.2.2
 pip install -r requirements.txt
 curl https://eu.ftp.opendatasoft.com/bmoreau/data_dumps.zip -L -o data_dumps.zip
 unzip -o data_dumps.zip
+rm data_dumps.zip
+rm -r __MACOSX/
