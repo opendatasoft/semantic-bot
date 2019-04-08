380a61aa718b0342b09cc4df835b62f80fe57774
benjimor <benji_moreau@hotmail.fr>
2019-04-08 15:05:39 +0200

    update tests on conversation_engine

--- tests/chatbot/test_conversation_engine.py
+++ tests/chatbot/test_conversation_engine.py
@@ -1,14 +1,19 @@
 from chatbot import conversation_engine as ConversationEngine
 
+URI = 'http://www.example.org/Class'
+DOMAIN_URI = 'http://www.example.org/Property'
+
 
 class TestConversationEngine(object):
 
     def test_complete_conversation(self):
         assert ConversationEngine.greeting()
         assert ConversationEngine.instructions()
-        assert ConversationEngine.class_question('field_name', 'class_description', 'uri')
-        assert ConversationEngine.property_question('field_name', 'predicate_description', 'uri')
-        assert ConversationEngine.property_class_question('field_name', 'predicate_description', 'uri')
+        assert ConversationEngine.class_question('field_name', 'class_description', URI)
+        assert ConversationEngine.property_question('field_name', 'predicate_description', URI, DOMAIN_URI, 'domain_description')
+        assert ConversationEngine.property_question('field_name', 'predicate_description', URI, None, None)
+        assert ConversationEngine.property_class_question('predicate_description', DOMAIN_URI, 'domain_description')
+        assert ConversationEngine.property_class_question('predicate_description', None, None)
         assert ConversationEngine.salutation()
 
     def test_conversation_errors(self):
