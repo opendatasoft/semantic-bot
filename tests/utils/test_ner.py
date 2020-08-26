dd27479ef045ebc9e0ec486dae637ba8b6596133
benjimor <benji_moreau@hotmail.fr>
2020-08-26 11:50:52 +0200

    try to fix test

--- tests/utils/test_ner.py
+++ tests/utils/test_ner.py
@@ -1,8 +1,9 @@
 from utils import virtuoso_ner as NER
 
 
-class TestDBpediaNER(object):
+class TestNER(object):
 
     def test_ner(self):
         class_list = NER.entity_types_request('Tiberius')
+        print(class_list)
         assert class_list
