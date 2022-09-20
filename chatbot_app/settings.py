53c1b0c266f17f224989bfbdac6705f760da8087
Kevin Roy <kiniou@gmail.com>
2022-09-20 09:51:49 +0200

    fix collectstatic and /static path

--- chatbot_app/settings.py
+++ chatbot_app/settings.py
@@ -151,7 +151,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
 )
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 WEBPACK_LOADER = {
